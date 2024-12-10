905b14780ca0beeb5c83dddc7f64342acc9f271d
Romain <romain.corbel@hotmail.com>
2024-12-10 15:47:57 +0100

    logistic

--- utils/logistic_regression.py
+++ utils/logistic_regression.py
@@ -7,19 +7,19 @@ def optimize_logistic_regression(X, y):
     param_grid = [
         {
             'penalty': ['l1', 'l2'],
-            'C': np.logspace(-6, 6, 20),
+            'C': np.logspace(-6, 6, 10),
             'solver': ['liblinear'],
             'max_iter': [100, 1000]
         },
         {
             'penalty': ['l2'],
-            'C': np.logspace(-6, 6, 20),
+            'C': np.logspace(-6, 6, 10),
             'solver': ['lbfgs', 'sag', 'newton-cg'],
             'max_iter': [100, 1000]
         },
         {
             'penalty': ['elasticnet'],
-            'C': np.logspace(-6, 6, 20),
+            'C': np.logspace(-6, 6, 10),
             'solver': ['saga'],
             'l1_ratio': [0.1, 0.5, 0.9],
             'max_iter': [100, 1000]
@@ -33,7 +33,7 @@ def optimize_logistic_regression(X, y):
     clf = GridSearchCV(
         LogisticRegression(class_weight='balanced'),
         param_grid=param_grid,
-        cv=3,
+        cv=10,
         scoring='f1',
         verbose=1,
         n_jobs=-1
